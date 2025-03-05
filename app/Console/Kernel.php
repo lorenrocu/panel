a6f3a50dcdaa35863c480074958684894e08c082
Lorenzo Romero Cubas <35782843+lorenrocu@users.noreply.github.com>
2025-03-05 12:04:56 -0500

    ejecutar actualizacion de databse cada 30 minutos

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,8 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         $schedule->command('tabla:vaciar-ingresos')->dailyAt('00:00');
         $schedule->command('programaciones:ejecutar')->everyMinute();
-        $schedule->command('chatwoot:update-config')->cron('0 */3 * * *');
+        $schedule->command('chatwoot:update-config')->everyThirtyMinutes();
+
     }
 
     /**
